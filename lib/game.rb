67e9c98e934e93b8517875a72735f719a8abf602
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-10-28 23:52:47 -0500

    Update UI to reflect game state

--- lib/game.rb
+++ lib/game.rb
@@ -6,28 +6,54 @@ class Game
     @game_state = [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
     @player1 = player1
     @player2 = player2
+    @player_to_move = @player1
   end
 
   def play_game
+    explain_game
     play_round
   end
 
   private
 
+  def explain_game
+    puts "On your turn, enter the row and column you want to select. For example, if\n"
+    puts "you want to select the example cell below, you would enter 3 and then 2."
+    display([[nil, nil, nil], [nil, nil, nil], [nil, "X", nil]])
+  end
+
   def play_round
-    p1_move = @player1.ask_move
-    update_game(p1_move)
-    display
-    p2_move = @player2.ask_move
-    update_game(p2_move)
-    display
+    player_move = @player_to_move.ask_move
+    update_game(player_move)
+    @player_to_move = @player_to_move == @player1 ? @player2 : @player1
+    display(@game_state)
   end
 
   def update_game(move)
     @game_state[move[0] - 1][move[1] - 1] = move[2]
   end
 
-  def display
-    p @game_state
+  def display(game_state)
+    display_row([1, 2, 3], 0)
+    game_state.each_with_index do |row, i|
+      14.times { print "-" }
+      puts "-"
+      display_row(row, i + 1)
+    end
+    14.times { print "-" }
+    puts "-"
+  end
+
+  def display_row(row, row_num)
+    print "#{row_num} "
+    row.each do |char|
+      print "| "
+      if char.nil?
+        print "  "
+      else
+        print "#{char} "
+      end
+    end
+    puts "|"
   end
 end
