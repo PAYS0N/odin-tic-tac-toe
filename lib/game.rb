0dc6487e61c0aee0f0ac67cbebebd0e4b3a763f7
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-10-29 01:12:06 -0500

    Implement functions to check if game is over

--- lib/game.rb
+++ lib/game.rb
@@ -11,7 +11,8 @@ class Game
 
   def play_game
     explain_game
-    play_round
+    game_over = play_round until game_over
+    end_game
   end
 
   private
@@ -27,8 +28,63 @@ class Game
     update_game(player_move)
     @player_to_move = @player_to_move == @player1 ? @player2 : @player1
     display(@game_state)
+    game_over?(player_move)
   end
 
+  def game_over?(move)
+    row = move[0] - 1
+    col = move[1] - 1
+    sym = move[2]
+    check_row(row, sym) || check_col(col, sym) || check_diagonals([row, col], sym)
+  end
+
+  def check_row(row, player_char)
+    game_over = true
+    @game_state[row].each do |char|
+      game_over = false unless char == player_char
+    end
+    game_over
+  end
+
+  def check_col(col, player_char)
+    game_over = true
+    @game_state.map { |arr| arr[col] }.each do |char|
+      game_over = false unless char == player_char
+    end
+    game_over
+  end
+
+  def check_diagonals(move, sym)
+    if move == [1, 1] && check_both_diags(sym)
+      true
+    else
+      (move[0] == move[1] && check_left_diag(sym)) || ([[0, 2], [2, 0]].include?(move) && check_right_diag(sym))
+    end
+  end
+
+  def check_both_diags(char)
+    return true if check_left_diag(char)
+
+    check_right_diag(char)
+  end
+
+  def check_left_diag(char)
+    game_over = true
+    (0..2).each do |i|
+      game_over = false unless @game_state[i][i] == char
+    end
+    game_over
+  end
+
+  def check_right_diag(char)
+    game_over = true
+    (0..2).each do |i|
+      game_over = false unless @game_state[i][2 - i] == char
+    end
+    game_over
+  end
+
+  # check if move is legal
   def update_game(move)
     @game_state[move[0] - 1][move[1] - 1] = move[2]
   end
@@ -55,5 +111,9 @@ class Game
       end
     end
     puts "|"
+  end
+
+  def end_game
+    puts "Game Over"
   end
 end
