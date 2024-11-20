4076890917e2b3eb3570282a2e6a20e321acbb0e
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-20 15:48:58 -0600

    Add tests for #play_round

---
+++ spec/game_engine_spec.rb
@@ -0,0 +1,134 @@
+# frozen_string_literal: true
+
+require_relative("../lib/game_engine")
+
+describe GameEngine do
+  let(:player1) { instance_double("player") }
+  let(:player2) { instance_double("player") }
+  subject(:game) { described_class.new(player1, player2) }
+
+  context "#play_game" do
+    context "on call" do
+      before do
+        allow(game).to receive(:explain_game)
+        allow(game).to receive(:play_round).and_return(true)
+        allow(game).to receive(:end_game)
+      end
+
+      it "sends call to explain game" do
+        expect(game).to receive(:explain_game)
+        game.play_game
+      end
+
+      it "sends call to end game" do
+        expect(game).to receive(:end_game)
+        game.play_game
+      end
+    end
+    context "when first round ends game" do
+      before do
+        allow(game).to receive(:explain_game)
+        allow(game).to receive(:play_round).and_return(true)
+        allow(game).to receive(:end_game)
+      end
+
+      it "calls play_round once" do
+        expect(game).to receive(:play_round).once
+        game.play_game
+      end
+    end
+
+    context "when second round ends game" do
+      before do
+        allow(game).to receive(:explain_game)
+        allow(game).to receive(:play_round).and_return(false, true)
+        allow(game).to receive(:end_game)
+      end
+
+      it "calls play_round twice" do
+        expect(game).to receive(:play_round).twice
+        game.play_game
+      end
+    end
+
+    context "when nth round ends game and n < 9" do
+      before do
+        allow(game).to receive(:explain_game)
+        allow(game).to receive(:play_round).and_return(false, false, false, false, false, true)
+        allow(game).to receive(:end_game)
+      end
+
+      it "calls play_round n times" do
+        expect(game).to receive(:play_round).exactly(6).times
+        game.play_game
+      end
+    end
+
+    context "when round never ends game" do
+      before do
+        allow(game).to receive(:explain_game)
+        allow(game).to receive(:play_round).and_return(false)
+        allow(game).to receive(:end_game)
+      end
+
+      it "calls play_round 9 times" do
+        expect(game).to receive(:play_round).exactly(9).times
+        game.play_game
+      end
+    end
+  end
+
+  context "#play_round" do
+    let(:checker) { instance_double("gameover_checker") }
+
+    before do
+      allow(game).to receive(:grab_move)
+      allow(game).to receive(:update_game)
+      allow(game).to receive(:display)
+      game.instance_variable_set(:@gameover_checker, checker)
+      allow(checker).to receive(:game_over?)
+    end
+
+    it "sends call to get player move" do
+      expect(game).to receive(:grab_move)
+      game.play_round
+    end
+
+    it "sends call to update game state" do
+      expect(game).to receive(:update_game)
+      game.play_round
+    end
+
+    context "when player1 went" do
+      before do
+        game.instance_variable_set(:@player_to_move, player1)
+      end
+
+      it "updates player to move to player 2" do
+        game.play_round
+        expect(game.instance_variable_get(:@player_to_move)).to eq(player2)
+      end
+    end
+
+    context "when player2 went" do
+      before do
+        game.instance_variable_set(:@player_to_move, player2)
+      end
+
+      it "updates player to move to player 1" do
+        game.play_round
+        expect(game.instance_variable_get(:@player_to_move)).to eq(player1)
+      end
+    end
+
+    it "sends call to display game" do
+      expect(game).to receive(:display)
+      game.play_round
+    end
+
+    it "sends call to check if game over" do
+      expect(checker).to receive(:game_over?)
+      game.play_round
+    end
+  end
+end
